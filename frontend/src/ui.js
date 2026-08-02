a46510ce2958098caa1acf476882ff5600f4fdb9
shriya-ops <shriya@agentstraw.com>
2026-08-02 23:42:11 +0530

    Reset dangling canvas connection on Escape

--- frontend/src/ui.js
+++ frontend/src/ui.js
@@ -244,8 +244,7 @@ const openQuickInsert = useCallback((event) => {
 
   return (
     <div ref={reactFlowWrapper} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
-      <ReactFlow
-        onDoubleClick={openQuickInsert}
+      <ReactFlow key={flowKey} onDoubleClick={openQuickInsert}
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
